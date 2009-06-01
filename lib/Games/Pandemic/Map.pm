04240c574709e257f3cc265414f4d5b8b3ea066a
Jérôme Quelin <jquelin@gmail.com>
2009-06-01 17:07:48 +0200

    first take at map implementation

---
+++ lib/Games/Pandemic/Map.pm
@@ -0,0 +1,60 @@
+package Games::Pandemic::Map;
+
+use Games::Pandemic::City;
+use Games::Pandemic::Disease;
+use Moose;
+
+has '_cities' => (
+    is      => 'ro',
+    isa     => 'ArrayRef',
+    default => sub { [] },
+    writer  => '_set_cities',
+);
+has '_diseases' => (
+    is      => 'ro',
+    isa     => 'ArrayRef',
+    default => sub { [] },
+    writer  => '_set_diseases',
+);
+
+sub BUILD {
+    my $self = shift;
+
+    # build the diseases
+    my @diseases;
+    foreach my $d ( $self->_raw_diseases ) {
+        my ($name, $color, $nb) = @$d;
+        my $disease = Games::Pandemic::Disease->new(
+            name  => $name,
+            color => $color,
+            nbmax => $nb,
+            _map  => $self,
+        );
+        push @diseases, $disease;
+    }
+    $self->_set_diseases( \@diseases );
+
+    # build the cities
+    my @cities;
+    foreach my $d ( $self->_raw_cities ) {
+        my ($name, $disid, $coords, $neighbours) = @$d;
+        my ($x, $y) = @$coords;
+        my $disease = $diseases[$disid];
+        my $city = Games::Pandemic::City->new(
+            name    => $name,
+            x       => $x,
+            y       => $y,
+            disease => $disease,
+            _map    => $self,
+        );
+        push @cities, $city;
+    }
+    $self->_set_cities( \@cities );
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__

e31a8e7d3de86560414903ae5a6ab73d9e0704a8
Jérôme Quelin <jquelin@gmail.com>
2009-06-01 17:42:21 +0200

    using moosex::attributehelpers to flatten the list of neighbours

--- lib/Games/Pandemic/City.pm
+++ lib/Games/Pandemic/City.pm
@@ -1,6 +1,7 @@
 package Games::Pandemic::City;
 
 use Moose;
+use MooseX::AttributeHelpers;
 
 has 'name'    => ( is => 'ro', required => 1, isa => 'Str' );
 has 'x'       => ( is => 'ro', required => 1, isa => 'Num' );
@@ -8,16 +9,21 @@ has 'y'       => ( is => 'ro', required => 1, isa => 'Num' );
 has 'disease' => ( is => 'ro', required => 1, isa => 'Games::Pandemic::Disease', weak_ref => 1 );
 has '_map'    => ( is => 'ro', required => 1, isa => 'Games::Pandemic::Map', weak_ref => 1 );
 has 'nb'      => ( is => 'rw', default  => 0, isa => 'Int' );
+
 has 'neighbour_ids' => (
-    is => 'ro',
-    required => 1,
-    isa => 'ArrayRef',
+    metaclass => 'Collection::Array',
+    is        => 'ro',
+    required  => 1,
+    isa       => 'ArrayRef',
+    provides  => {
+        elements => '_neighbour_ids',
+    },
 );
 
 sub neighbours {
     my $self = shift;
     my $map = $self->_map;
-    return map { $map->city_from_id($_) } $self->neighbour_ids;
+    return map { $map->city_from_id($_) } $self->_neighbour_ids;
 }
 
 no Moose;

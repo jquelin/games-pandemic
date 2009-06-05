f51db4ef7f9b820017415efcc7386ae0632e8a06
Jérôme Quelin <jquelin@gmail.com>
2009-06-05 19:10:00 +0200

    new attributes xreal/yreal

--- lib/Games/Pandemic/City.pm
+++ lib/Games/Pandemic/City.pm
@@ -6,6 +6,8 @@ use MooseX::AttributeHelpers;
 has 'name'    => ( is => 'ro', required => 1, isa => 'Str' );
 has 'x'       => ( is => 'ro', required => 1, isa => 'Num' );
 has 'y'       => ( is => 'ro', required => 1, isa => 'Num' );
+has 'xreal'   => ( is => 'ro', required => 1, isa => 'Num' );
+has 'yreal'   => ( is => 'ro', required => 1, isa => 'Num' );
 has 'disease' => ( is => 'ro', required => 1, isa => 'Games::Pandemic::Disease', weak_ref => 1 );
 has '_map'    => ( is => 'ro', required => 1, isa => 'Games::Pandemic::Map', weak_ref => 1 );
 has 'nb'      => ( is => 'rw', default  => 0, isa => 'Int' );

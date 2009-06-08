c304932b6472881f3dd79d8f987b74c4653f73c9
Jérôme Quelin <jquelin@gmail.com>
2009-06-08 19:57:09 +0200

    creating menu bar

--- lib/Games/Pandemic/Tk/Main.pm
+++ lib/Games/Pandemic/Tk/Main.pm
@@ -15,12 +15,53 @@ Readonly my $mw => $poe_main_window; # already created by poe
 # -- initialization
 
 sub START {
-    my $self = shift;
+    my ($self, $session) = @_[OBJECT, SESSION];
     $K->alias_set('main');
+    $self->_build_gui($session);
 }
 
 # -- public events
 
+# -- gui creation
+
+
+sub _build_gui {
+    my ($self, $session) = @_;
+    $self->_build_menu($session);
+}
+
+sub _build_menu {
+    my ($self, $s) = @_;
+
+    # no tear-off menus
+    $mw->optionAdd('*tearOff', 'false');
+
+    # create the menu for the window
+    my $menubar = $mw->Menu;
+    $mw->configure(-menu => $menubar );
+
+    # menu game
+    my $game = $menubar->cascade(-label => '~Game');
+    $game->command(
+        -label       => '~Close',
+        -accelerator => 'Ctrl+W',
+        -command     => $s->postback('_window_close'),
+        -image       => $mw->Photo('fileclose16'),
+        -compound    => 'left',
+    );
+    $mw->bind('<Control-w>', $s->postback('_window_close'));
+    $mw->bind('<Control-W>', $s->postback('_window_close'));
+
+    $game->command(
+        -label       => '~Quit',
+        -accelerator => 'Ctrl+Q',
+        -command     => $s->postback('_quit'),
+        -image       => $mw->Photo('actexit16'),
+        -compound    => 'left',
+    );
+    $mw->bind('<Control-q>', $s->postback('_quit'));
+    $mw->bind('<Control-Q>', $s->postback('_quit'));
+}
 
 1;
 __END__

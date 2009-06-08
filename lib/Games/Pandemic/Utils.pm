5a7f071dc5fb3c3f7bbb94b74371d013df4ba3c7
Jérôme Quelin <jquelin@gmail.com>
2009-06-08 19:02:51 +0200

    exporting $SHAREDIR from gp:utils

--- lib/Games/Pandemic/Utils.pm
+++ lib/Games/Pandemic/Utils.pm
@@ -3,18 +3,38 @@ package Games::Pandemic::Utils;
 use 5.010;
 use Moose;
 use Devel::CheckOS        qw{ os_is };
+use File::Basename        qw{ fileparse };
 use File::HomeDir         qw{ my_data };
 use File::Spec::Functions qw{ catdir rel2abs };
+use Module::Util          qw{ find_installed };
 use Readonly;
  
 extends 'Exporter';
-our @EXPORT = qw{ $CONFIG_DIR };
+our @EXPORT = qw{ $CONFIG_DIR $SHAREDIR };
 
 Readonly our $CONFIG_DIR => rel2abs( catdir(
     my_data(),
     ( os_is('MicrosoftWindows' ) ? 'Perl' : '.perl' ),
     'Games-Pandemic',
 ) );
+
+
+Readonly our $SHAREDIR => _find_share_dir();
+
+
+
+# -- private subs
+
+#
+# my $path = _find_share_dir();
+#
+# return the absolute path where all resources will be placed.
+#
+sub _find_share_dir {
+    my $path = find_installed(__PACKAGE__);
+    my ($undef, $dirname) = fileparse($path);
+    return rel2abs( catdir($dirname, 'share') );
+}
 
 
 1;

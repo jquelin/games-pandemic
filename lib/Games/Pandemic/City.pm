5d7f2403109533f1a26d7759c1957fe296b0a9e7
Jérôme Quelin <jquelin@gmail.com>
2009-06-09 14:08:56 +0200

    cleaned attribute definition

--- lib/Games/Pandemic/City.pm
+++ lib/Games/Pandemic/City.pm
@@ -5,16 +5,16 @@ use Moose;
 use MooseX::AttributeHelpers;
 use MooseX::FollowPBP;
 
-has 'name'    => ( is => 'ro', required => 1, isa => 'Str' );
-has 'x'       => ( is => 'ro', required => 1, isa => 'Num' );
-has 'y'       => ( is => 'ro', required => 1, isa => 'Num' );
-has 'xreal'   => ( is => 'ro', required => 1, isa => 'Num' );
-has 'yreal'   => ( is => 'ro', required => 1, isa => 'Num' );
-has 'disease' => ( is => 'ro', required => 1, isa => 'Games::Pandemic::Disease', weak_ref => 1 );
-has '_map'    => ( is => 'ro', required => 1, isa => 'Games::Pandemic::Map', weak_ref => 1 );
-has 'nb'      => ( is => 'rw', default  => 0, isa => 'Int' );
+has name    => ( is => 'ro', required => 1, isa => 'Str' );
+has x       => ( is => 'ro', required => 1, isa => 'Num' );
+has y       => ( is => 'ro', required => 1, isa => 'Num' );
+has xreal   => ( is => 'ro', required => 1, isa => 'Num' );
+has yreal   => ( is => 'ro', required => 1, isa => 'Num' );
+has disease => ( is => 'ro', required => 1, isa => 'Games::Pandemic::Disease', weak_ref => 1 );
+has _map    => ( is => 'ro', required => 1, isa => 'Games::Pandemic::Map', weak_ref => 1 );
+has nb      => ( is => 'rw', default  => 0, isa => 'Int' );
 
-has 'neighbour_ids' => (
+has neighbour_ids => (
     metaclass => 'Collection::Array',
     is        => 'ro',
     required  => 1,
@@ -26,7 +26,7 @@ has 'neighbour_ids' => (
 
 sub neighbours {
     my $self = shift;
-    my $map = $self->_map;
+    my $map = $self->_get_map;
     return map { $map->city_from_id($_) } $self->_neighbour_ids;
 }
 

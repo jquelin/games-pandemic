e68be18cb9f68595bb3c725cac32ddf7d5348716
Jérôme Quelin <jquelin@gmail.com>
2009-06-10 11:31:40 +0200

    new role: medic

---
+++ lib/Games/Pandemic/Role/Medic.pm
@@ -0,0 +1,24 @@
+package Games::Pandemic::Role::Medic;
+# ABSTRACT: medic pandemic role
+
+use 5.010;
+use Locale::TextDomain 'Games-Pandemic';
+use Moose;
+use MooseX::FollowPBP;
+
+extends 'Games::Pandemic::Role';
+
+
+# -- default builders
+
+sub _auto_clean_on_cure_builder { 1 }
+sub _color_builder              { '#79af00' }
+sub _cure_all_builder           { 1 }
+sub _role_name_builder          { __('Medic') }
+
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
+__END__

4225f13a7f6e7db4557a1be95efb49c882454fa5
Jérôme Quelin <jquelin@gmail.com>
2009-06-11 13:41:40 +0200

    new method image()

--- lib/Games/Pandemic/Disease.pm
+++ lib/Games/Pandemic/Disease.pm
@@ -1,9 +1,12 @@
 package Games::Pandemic::Disease;
 # ABSTRACT: disease object for Games::Pandemic
 
+use File::Spec::Functions qw{ catfile };
 use Moose;
 use MooseX::AttributeHelpers;
 use MooseX::SemiAffordanceAccessor;
+
+# -- attributes
 
 has 'colors' => (
     metaclass  => 'Collection::List',
@@ -17,6 +20,21 @@ has 'name'  => ( is => 'ro', required => 1 );
 has 'nb'    => ( is => 'rw', default  => 0, isa => 'Int' );
 has 'nbmax' => ( is => 'ro', required => 1, isa => 'Int' );
 has '_map'  => ( is => 'ro', required => 1, isa => 'Games::Pandemic::Map', weak_ref => 1 );
+
+
+# -- public methods
+
+=method my $path = $disease->image;
+
+Return the C<$path> to an image for the disease B<cure>.
+
+=cut
+
+sub image {
+    my $self = shift;
+    return catfile( $self->_map->sharedir, $self->_image );
+}
+
 
 no Moose;
 __PACKAGE__->meta->make_immutable;

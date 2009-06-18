dce6e3d55f28de1ee9dc264103f352e69022c3e9
Jérôme Quelin <jquelin@gmail.com>
2009-06-18 17:36:45 +0200

    new class gp:card

---
+++ lib/Games/Pandemic/Card.pm
@@ -0,0 +1,17 @@
+package Games::Pandemic::Card;
+# ABSTRACT: base class for pandemic cards
+
+use 5.010;
+use Moose;
+use MooseX::SemiAffordanceAccessor;
+
+# -- accessors
+
+has label => ( is => 'ro', isa => 'Str', lazy_build => 1 );
+
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
+__END__

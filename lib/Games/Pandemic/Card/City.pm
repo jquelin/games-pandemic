5c2811e23e5aef6bd1659118a119e3f316492fae
Jérôme Quelin <jquelin@gmail.com>
2009-06-18 17:37:14 +0200

    new card type: city

---
+++ lib/Games/Pandemic/Card/City.pm
@@ -0,0 +1,31 @@
+package Games::Pandemic::Card::City;
+# ABSTRACT: pandemic city card
+
+use 5.010;
+use Moose;
+use MooseX::SemiAffordanceAccessor;
+
+extends 'Games::Pandemic::Card';
+
+# -- accessors
+
+has city => (
+    is => 'ro',
+    isa => 'Games::Pandemic::City',
+    weak_ref => 1,
+);
+
+
+# -- default builders
+
+sub _build_label {
+    my $self = shift;
+    return $self->city->name;
+}
+
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
+__END__

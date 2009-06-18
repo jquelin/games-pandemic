4fb29b66fa654b9a693618ed480524b5d7a1d2fb
Jérôme Quelin <jquelin@gmail.com>
2009-06-18 17:39:30 +0200

    new class gp:deck

---
+++ lib/Games/Pandemic/Deck.pm
@@ -0,0 +1,54 @@
+package Games::Pandemic::Deck;
+# ABSTRACT: pandemic card deck
+
+use 5.010;
+use Moose;
+use MooseX::AttributeHelpers;
+use MooseX::SemiAffordanceAccessor;
+
+
+# -- accessors
+
+has cards => (
+    is => 'rw',
+    isa => 'ArrayRef[Games::Pandemic::Card]',
+    auto_deref => 1,
+);
+
+has _deck => (
+    metaclass  => 'Collection::Array',
+    isa        => 'ArrayRef[Games::Pandemic::Card]',
+    default    => sub { [] },
+    auto_deref => 1,
+    provides   => {
+        clear => '_clear_deck',
+        pop   => 'next',
+        shift => 'last',
+    },
+);
+
+has _pile => (
+    metaclass  => 'Collection::Array',
+    isa        => 'ArrayRef[Games::Pandemic::Card]',
+    default    => sub { [] },
+    auto_deref => 1,
+    provides   => {
+        clear    => '_clear_pile',
+        push     => 'discard',
+        elements => 'past',
+    },
+);
+
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
+__END__
+
+
+=head1 DESCRIPTION
+
+A C<Games::Pandemic::Deck> contains 2 sets of C<Games::Pandemic::Card>:
+a drawing deck and a discard pile.
+

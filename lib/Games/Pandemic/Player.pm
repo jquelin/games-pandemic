146c4e405e25d9311ec96f73d5dac82e88f6eba9
Jérôme Quelin <jquelin@gmail.com>
2009-06-21 16:34:22 +0200

    new attributes for player: role_class and role

--- lib/Games/Pandemic/Player.pm
+++ lib/Games/Pandemic/Player.pm
@@ -7,12 +7,28 @@ use warnings;
 
 use Moose;
 use MooseX::SemiAffordanceAccessor;
+use UNIVERSAL::require;
 
 
 # -- accessors
 
+has role_class => ( is=>'ro', isa=>'Str', required => 1, );
+has role       => ( is=>'rw', isa=>'Games::Pandemic::Role', lazy_build=>1 );
+
+
 # -- default builders
 
+sub _build_role {
+    my $self = shift;
+
+    # load the wanted role module
+    my $class = 'Games::Pandemic::Role::' . $self->role_class;
+    $class->require;
+
+    # create the new role and store it
+    my $role = $class->new;
+    $self->set_role($role);
+}
 
 
 no Moose;

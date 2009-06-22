8158c2fa5821d41096f81c62599f4633814ebe6d
Jérôme Quelin <jquelin@gmail.com>
2009-06-22 19:42:51 +0200

    player icon allows to hide/show the cards

--- lib/Games/Pandemic/Tk/PlayerFrame.pm
+++ lib/Games/Pandemic/Tk/PlayerFrame.pm
@@ -6,6 +6,7 @@ use strict;
 use warnings;
 
 use Moose;
+use MooseX::AttributeHelpers;
 use MooseX::SemiAffordanceAccessor;
 use Tk;
 
@@ -27,6 +28,11 @@ has player => (
     weak_ref => 1,
 );
 
+has _button => (
+    is => 'rw',
+    isa => 'Tk::Button',
+    weak_ref => 1,
+);
 has _frame => (
     is => 'rw',
     isa => 'Tk::Frame',
@@ -34,6 +40,25 @@ has _frame => (
     lazy_build => 1,
     handles => [ qw{ pack } ],
 );
+has _fcards => (
+    is => 'rw',
+    isa => 'Tk::Frame',
+    weak_ref => 1,
+);
+
+has _is_opened => (
+      metaclass => 'Bool',
+      is        => 'rw',
+      isa       => 'Bool',
+      default   => 1,
+      provides  => {
+          #set     => 'illuminate',
+          #unset   => 'darken',
+          toggle  => '_switch_open',
+          #not     => 'is_dark'
+      }
+);
+
 
 
 # -- initialization
@@ -43,9 +68,14 @@ sub _build__frame {
     my $parent = $self->parent;
     my $f = $parent->Frame;
 
-    $f->Label(
+    my $but = $f->Button(
         -image => $parent->Photo( -file=>$self->player->role->image('icon', 32) ),
+        -command => sub { $self->_toggle_visibility },
     )->pack(@LEFT);
+    $self->_set_button($but);
+
+    my $fcards = $f->Frame->pack(@LEFT, @FILLY);
+    $self->_set_fcards($fcards);
 
     return $f;
 }
@@ -55,12 +85,23 @@ sub _build__frame {
 
 sub add_card {
     my ($self, $card) = @_;
-
-    my $f = $self->_frame->Frame->pack(@LEFT);
+    my $f = $self->_fcards;
     $f->Label(-image => $self->parent->Photo(-file=>($card->icon)))->pack(@LEFT);
     $f->Label(-text => $card->label, -anchor=>'w')->pack(@LEFT);
 }
 
+
+# -- private methods
+
+sub _toggle_visibility {
+    my $self = shift;
+    if ( $self->_is_opened ) {
+        $self->_fcards->packForget;
+    } else {
+        $self->_fcards->pack(@LEFT, @FILLY);
+    }
+    $self->_switch_open;
+}
 
 no Moose;
 __PACKAGE__->meta->make_immutable;

ac26ac03d77e58566e0a9ce20658c76437190e64
Jérôme Quelin <jquelin@gmail.com>
2009-06-22 17:51:16 +0200

    easy way to create 3 more players

--- lib/Games/Pandemic/Controller.pm
+++ lib/Games/Pandemic/Controller.pm
@@ -71,6 +71,9 @@ event new_game => sub {
     # FIXME: initial number of card depends of map / number of players
     $K->yield( _new_player => 'Scientist', 4 );
     $K->yield( _new_player => 'OperationsExpert', 4 );
+#    $K->yield( _new_player => 'Medic', 4 );
+#    $K->yield( _new_player => 'Researcher', 4 );
+#    $K->yield( _new_player => 'Dispatcher', 4 );
 
     # signal main window that we have started a new game
     $K->post( 'main' => 'new_game' );

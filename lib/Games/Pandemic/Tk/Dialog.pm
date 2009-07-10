615f5dd1c1fdcde2fbe8054d95590a87300d53d8
Jérôme Quelin <jquelin@gmail.com>
2009-07-10 17:10:17 +0200

    pod update

--- lib/Games/Pandemic/Tk/Dialog.pm
+++ lib/Games/Pandemic/Tk/Dialog.pm
@@ -16,8 +16,8 @@ use Games::Pandemic::Tk::Utils;
 # -- accessors
 
 has parent => ( is=>'ro', required=>1, weak_ref=>1, isa=>'Tk::Widget' );
-has header => ( is=>'rw', isa=>'Str', lazy_build=>1 );
 has title  => ( is=>'rw', isa=>'Str', lazy_build=>1 );
+has header => ( is=>'rw', isa=>'Str', lazy_build=>1 );
 has _toplevel => ( is=>'rw', isa=>'Tk::Toplevel' );
 
 
@@ -113,3 +113,30 @@ __END__
 BUILD
 
 =end Pod::Coverage
+
+
+=head1 DESCRIPTION
+
+C<Games::Pandemic::Tk::Dialog> is meant to be used as a base class for
+Pandemic dialogs, and should not be used directly.
+
+It will create a new toplevel with the Pandemic icon, a title and
+possibly a header.
+
+It accepts the following attributes:
+
+=over 4
+
+=item * parent - the parent window of the dialog, required
+
+=item * title - the dialog title, default to C<Pandemic>
+
+=item * header - a header to display at the top of the window, no default
+
+=back
+
+To subclass it, declare your own attributes, create the lazy builders
+for the attributes, and C<augment> the C<_build_gui()> method to create
+the bottom of the dialog window.
+
+

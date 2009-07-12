4fb3d8cc75201809883e7bd5c4bcf518d6a9af25
Jérôme Quelin <jquelin@gmail.com>
2009-07-12 14:41:08 +0200

    check too_many_cards when dealing cards

--- lib/Games/Pandemic/Controller.pm
+++ lib/Games/Pandemic/Controller.pm
@@ -442,7 +442,12 @@ event _deal_card => sub {
     }
 
     # FIXME: game over if no more card
-    # FIXME: if player has too much cards
+
+    # check if player has too much cards
+    if ( $player->nb_cards > $player->max_cards ) {
+        $game->set_too_many_cards($player);
+        $K->post( main => 'too_many_cards', $player );
+    }
 };
 
 

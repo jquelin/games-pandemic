56d96e51215f0c9f668e90200889aad95c84b53b
Jérôme Quelin <jquelin@gmail.com>
2009-07-25 17:23:46 +0200

    creating a basic toplevel

--- lib/Games/Pandemic/Tk/PlayerCards.pm
+++ lib/Games/Pandemic/Tk/PlayerCards.pm
@@ -6,10 +6,16 @@ use strict;
 use warnings;
 
 use Moose;
+use MooseX::SemiAffordanceAccessor;
 use Tk;
 
 use Games::Pandemic::Tk::Utils;
 use Games::Pandemic::Utils;
+
+# -- attributes & accessors
+
+has parent    => ( is=>'ro', required=>1, weak_ref=>1, isa=>'Tk::Widget' );
+has _toplevel => ( is=>'rw', isa=>'Tk::Toplevel' );
 
 
 # -- initialization
@@ -21,6 +27,44 @@ use Games::Pandemic::Utils;
 #
 sub BUILD {
     my $self = shift;
+    $self->_build_gui;
+}
+
+
+#
+# DEMOLISH()
+#
+# called as destructor
+#
+sub DEMOLISH {
+    my $self = shift;
+    debug( "~player cards: $self\n" );
+}
+
+
+
+# -- private methods
+
+#
+# dialog->_build_gui;
+#
+# create the various gui elements.
+#
+sub _build_gui {
+    my $self = shift;
+    my $parent = $self->parent;
+
+    my $top = $parent->Toplevel;
+    $self->_set_toplevel($top);
+    $top->withdraw;
+
+    # window title
+    $top->title( T('Cards') );
+    $top->iconimage( pandemic_icon($top) );
+
+    # center window & make it appear
+    $top->Popup( -popover => $parent );
+    $top->resizable(0,0);
 }
 
 no Moose;
@@ -32,5 +76,6 @@ __END__
 =begin Pod::Coverage
 
 BUILD
+DEMOLISH
 
 =end Pod::Coverage

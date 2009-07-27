12b9c3e318b1684c90c561ebb655559b559e3ef2
Jérôme Quelin <jquelin@gmail.com>
2009-07-27 14:56:13 +0200

    introducing gp:tk:action

---
+++ lib/Games/Pandemic/Tk/Action.pm
@@ -0,0 +1,78 @@
+package Games::Pandemic::Tk::Action;
+# ABSTRACT: action item for main Games::Pandemic window
+
+use 5.010;
+use strict;
+use warnings;
+
+use Moose;
+use MooseX::SemiAffordanceAccessor;
+
+use Games::Pandemic::Tk::Utils;
+use Games::Pandemic::Utils;
+
+
+# -- attributes & accessors
+
+# a hash with action widgets.
+has _widgets => (
+    metaclass => 'Collection::Hash',
+    is        => 'ro',
+    isa       => 'HashRef',
+    default   => sub { {} },
+    provides  => {
+        delete  => 'rm_widget',
+        set     => '_set_widget',       # $action->_set_widget($widget, $widget);
+        values  => '_all_widgets',      # my @widgets = $action->_all_widgets;
+    },
+);
+
+
+# -- public methods
+
+=method $action->add_widget( $widget );
+
+Associate C<$widget> with C<$action>.
+
+=cut
+
+sub add_widget {
+    my ($self, $widget) = @_;
+    $self->_set_widget($widget, $widget);
+}
+
+
+=method $action->rm_widget( $widget );
+
+De-associate C<$widget> with C$<action>.
+
+=cut
+
+# rm_widget() implemented in _widget attribute declaration
+
+
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
+__END__
+
+
+=head1 SYNOPSIS
+
+    my $action = Games::Pandemic::Tk::Action->new;
+    $action->add_widget( $menu_entry );
+    $action->add_widget( $button );
+
+
+=head1 DESCRIPTION
+
+Menu entries are often also available in toolbars or other widgets. And
+sometimes, we want to enable or disable a given action, and this means
+having to update everything this action is allowed.
+
+This module helps managing actions in a GUI: just create a new object,
+associate some widgets with C<add_widget()> and then de/activate the
+whole action at once.
+

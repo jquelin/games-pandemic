b506d705d6227343f3a9d34dd522f931b0bddc8f
Jérôme Quelin <jquelin@gmail.com>
2009-08-23 19:08:38 +0200

    viewcards uses tiler for a nicer result

--- lib/Games/Pandemic/Tk/Dialog/ViewCards.pm
+++ lib/Games/Pandemic/Tk/Dialog/ViewCards.pm
@@ -9,6 +9,7 @@ use List::Util qw{ max };
 use Moose;
 use MooseX::SemiAffordanceAccessor;
 use Tk;
+use Tk::Tiler;
 
 extends 'Games::Pandemic::Tk::Dialog';
 
@@ -45,25 +46,24 @@ augment _build_gui => sub {
     my $top   = $self->_toplevel;
     my @cards = $self->cards;
 
-    # compute a minimum length, for a nice outlook on the eye. we do
-    # that by multiplying by the magic number 4, making the window
-    # neither too narrow nor too big. ymmv, but i like it this way! :-)
-    my $max   = max map { length $_->label } @cards;
-    my $width = $max * 4;
-
     # main elements
-    $top->Label(-text=>'(more recent)', -width=>$width)->pack(@TOP, @FILLX, @PAD2);
-    my $f = $top->Scrolled('Frame', -scrollbars=>'oe')->pack(@TOP, @XFILL2, @PAD2);
-    $top->Label(-text=>'(older)')->pack(@TOP, @FILLX, @PAD2);
+    my $tiler = $top->Scrolled( 'Tiler',
+        -scrollbars => 'oe',
+        -rows       => 8,
+        -columns    => 3,
+    )->pack(@TOP, @XFILL2, @PAD2);
+    #$tiler->Manage( $tiler->Label(-text=>T('(older)'), -anchor=>'w') );
 
     # display cards
     foreach my $card ( reverse @cards ) {
         # to display a checkbutton with image + text, we need to
         # create a checkbutton with a label just next to it.
-        my $fcard = $f->Frame->pack(@TOP, @FILLX);
+        my $fcard = $tiler->Frame;
         $fcard->Label( -image => image($card->icon, $top) )->pack(@LEFT);
         $fcard->Label( -text  => $card->label, -anchor => 'w' )->pack(@LEFT, @FILLX);
+        $tiler->Manage($fcard);
     }
+    #$tiler->Manage( $tiler->Label(-text=>T('(more recent)')) );
 };
 
 

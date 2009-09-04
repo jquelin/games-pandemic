c547195b8885ebecdedf89b63434441a38bbca95
Jérôme Quelin <jquelin@gmail.com>
2009-09-04 11:02:01 +0200

    new method event()

--- lib/Games/Pandemic/Card/Special.pm
+++ lib/Games/Pandemic/Card/Special.pm
@@ -10,6 +10,30 @@ use MooseX::SemiAffordanceAccessor;
 
 extends 'Games::Pandemic::Card';
 
+
+# -- public methods
+
+
+=method my $event = $special->event;
+
+Return the special card name, from C<CamelCaseName> to
+C<camel_case_name>. This will be used as the event name sent to
+main window.
+
+Eg, L<Games::Pandemic::Card::Special::OneQuietNight> objects will return
+C<one_quiet_night>.
+
+=cut
+
+sub event {
+    my $self = shift;
+    my $ref  = ref $self;
+    $ref =~ s/^Games::Pandemic::Card::Special:://;
+    $ref =~ s/([[:upper:]])/_$1/g;
+    $ref =~ s/^_//;
+    return lc $ref;
+}
+
 no Moose;
 __PACKAGE__->meta->make_immutable;
 

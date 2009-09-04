3000da71b0931bb303cf7fcc89f4d21aebc5bbcf
Jérôme Quelin <jquelin@gmail.com>
2009-09-04 14:46:17 +0200

    introducing gp:t:d:governmentgrant

---
+++ lib/Games/Pandemic/Tk/Dialog/GovernmentGrant.pm
@@ -0,0 +1,145 @@
+use 5.010;
+use strict;
+use warnings;
+
+package Games::Pandemic::Tk::Dialog::GovernmentGrant;
+# ABSTRACT: dialog window to play a government grant
+
+use File::Spec::Functions qw{ catfile };
+use Moose;
+use MooseX::SemiAffordanceAccessor;
+use POE;
+use Readonly;
+use Tk;
+use Tk::Tiler;
+
+extends 'Games::Pandemic::Tk::Dialog';
+
+use Games::Pandemic::Utils;
+use Games::Pandemic::Tk::Utils;
+
+Readonly my $K    => $poe_kernel;
+Readonly my $GREY => '#666666';
+
+
+# -- accessors
+
+has card => (
+    is         => 'rw',
+    isa        => 'Games::Pandemic::Card',
+    required   => 1,
+);
+
+has _selcard => (
+    is         => 'rw',
+    isa        => 'Games::Pandemic::Card',
+);
+
+has _selected => (
+    is         => 'rw',
+    isa        => 'ArrayRef[Tk::Widget]',
+    predicate  => '_has_selected',
+    auto_deref => 1,
+);
+
+# -- initialization
+
+sub _build_header    { $_[0]->card->label }
+sub _build_resizable { 1 }
+sub _build_title     { T('Special event') }
+sub _build__ok       { T('Build') }
+sub _build__cancel   { T('Cancel') }
+
+
+
+# -- private methods
+
+#
+# $main->_build_gui;
+#
+# create the various gui elements.
+#
+augment _build_gui => sub {
+    my $self = shift;
+    my $top  = $self->_toplevel;
+    my $card = $self->card;
+
+    # icon + text
+    my $f   = $top->Frame->pack(@TOP,@FILLX);
+    my $img  = image( catfile($SHAREDIR, 'cards', 'government-grant-48.png') );
+    $f->Label(-image => $img)->pack(@LEFT, @FILL2, @PAD10);
+    $f->Label(
+        -text       => $card->description,
+        -justify    => 'left',
+    )->pack(@LEFT, @FILLX, @PAD10);
+
+    # main elements
+    my $tiler = $top->Scrolled( 'Tiler',
+        -scrollbars => 'oe',
+        -rows       => 8,
+        -columns    => 3,
+    )->pack(@TOP, @XFILL2, @PAD2);
+
+    # display cards
+    my $game = Games::Pandemic->instance;
+    my @citycards =
+        sort { $a->city->disease->name cmp $b->city->disease->name
+            || $a->label cmp $b->label }
+        $game->map->disease_cards;
+    foreach my $card ( @citycards ) {
+        # to display a checkbutton with image + text, we need to
+        # create a checkbutton with a label just next to it.
+        my $fcard = $tiler->Frame;
+        my $img = $fcard->Label( -image => image($card->icon, $top) )->pack(@LEFT);
+        my $lab = $fcard->Label( -text  => $card->label, -anchor => 'w' )->pack(@LEFT, @FILLX);
+        $_->bind('<1>', [$self, '_select_city', $card, $fcard, $img, $lab] )
+            for ($img, $lab, $fcard);
+        $tiler->Manage($fcard);
+    }
+};
+
+
+# -- private methods
+
+
+sub _select_city {
+    my ($self, $card, $fcard, $img, $lab) = @_;
+
+    # store new selected card
+    $self->_set_selcard($card);
+
+    # remove previous selection
+    my $bg = $fcard->cget('-bg');
+    if ( $self->_has_selected ) {
+        my @old = $self->_selected;
+        $_->configure(-bg=>$bg) for @old;
+    }
+
+    # add selection
+    my @new = ($img, $lab, $fcard);
+    $self->_set_selected( \@new );
+    $_->configure(-bg=>$GREY) for @new;
+}
+
+
+sub _valid {
+    say "ici";
+}
+
+
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
+__END__
+
+=begin Pod::Coverage
+
+BUILD
+
+=end Pod::Coverage
+
+=head1 DESCRIPTION
+
+This dialog implements 

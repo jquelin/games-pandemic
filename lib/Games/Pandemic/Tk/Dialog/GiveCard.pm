e906df25e8b55ef2c9075792d84ff3bd0ce6cc49
Jérôme Quelin <jquelin@gmail.com>
2009-12-04 14:16:23 +0100

    using tk::sugar

--- lib/Games/Pandemic/Tk/Dialog/GiveCard.pm
+++ lib/Games/Pandemic/Tk/Dialog/GiveCard.pm
@@ -10,11 +10,12 @@ use MooseX::SemiAffordanceAccessor;
 use POE;
 use Readonly;
 use Tk;
+use Tk::Sugar;
 
 extends 'Games::Pandemic::Tk::Dialog';
 
+use Games::Pandemic::Tk::Utils qw{ image };
 use Games::Pandemic::Utils;
-use Games::Pandemic::Tk::Utils;
 
 Readonly my $K => $poe_kernel;
 
@@ -72,7 +73,7 @@ augment _build_gui => sub {
     my $self = shift;
     my $top  = $self->_toplevel;
 
-    my $fcenter = $top->Frame->pack(@TOP, @XFILL2);
+    my $fcenter = $top->Frame->pack(top, xfill2);
 
 
     # if more than one player, select which one will receive the card
@@ -80,28 +81,28 @@ augment _build_gui => sub {
     $self->_set_player( $players[0] );
     if ( @players > 1 ) {
         # enclose players in their own frame
-        my $f = $fcenter->Frame->pack(@LEFT, @PAD10, -anchor=>'nw');
+        my $f = $fcenter->Frame->pack(left, pad10, NW);
         $f->Label(
             -text   => T('Select player receiving the card:'),
             -anchor => 'w',
-        )->pack(@TOP, @FILLX);
+        )->pack(top, fillx);
 
         # display players
         my $selplayer = $self->_player->role;
         foreach my $player ( @players ) {
             # to display a radiobutton with image + text, we need to
             # create a radiobutton with a label just next to it.
-            my $fplayer = $f->Frame->pack(@TOP, @FILLX);
+            my $fplayer = $f->Frame->pack(top, fillx);
             my $rb = $fplayer->Radiobutton(
                 -text     => $player->role,
                 -variable => \$selplayer,
                 -value    => $player->role,
                 -anchor   => 'w',
                 -command  => sub{ $self->_set_player($player); },
-            )->pack(@LEFT, @XFILLX);
+            )->pack(left, xfillx);
             my $lab = $fplayer->Label(
                 -image    => image( $player->image('icon', 32), $top ),
-            )->pack(@LEFT);
+            )->pack(left);
             $lab->bind( '<1>', sub { $rb->invoke; } );
         }
     }
@@ -111,28 +112,28 @@ augment _build_gui => sub {
     $self->_set_card( $cards[0] );
     if ( @cards > 1 ) {
         # enclosed cards in their own frame
-        my $f = $fcenter->Frame->pack(@LEFT, @FILLX, @PAD10, -anchor=>'nw');
+        my $f = $fcenter->Frame->pack(left, fillx, pad10, NW);
         $f->Label(
             -text   => T('Select city card to give:'),
             -anchor => 'w',
-        )->pack(@TOP, @FILLX);
+        )->pack(top, fillx);
 
         # display cards
         my $selcard = $self->_card->label;
         foreach my $card ( @cards ) {
             # to display a radiobutton with image + text, we need to
             # create a radiobutton with a label just next to it.
-            my $fcity = $f->Frame->pack(@TOP, @FILLX);
+            my $fcity = $f->Frame->pack(top, fillx);
             my $rb = $fcity->Radiobutton(
                 -image    => image($card->icon, $top),
                 -variable => \$selcard,
                 -value    => $card->label,
                 -command  => sub { $self->_set_card($card); },
-            )->pack(@LEFT);
+            )->pack(left);
             my $lab = $fcity->Label(
                 -text   => $card->label,
                 -anchor => 'w',
-            )->pack(@LEFT, @FILLX);
+            )->pack(left, fillx);
             $lab->bind( '<1>', sub { $rb->invoke; } );
         }
     }

6419acbb1d0c31c024d48d2a71f96f8c26523033
Jérôme Quelin <jquelin@gmail.com>
2011-04-12 15:04:30 +0200

    i18n gets its own module

---
+++ lib/Games/Pandemic/I18N.pm
@@ -0,0 +1,14 @@
+use 5.010;
+use strict;
+use warnings;
+
+package Games::Pandemic::I18N;
+# ABSTRACT: internationalization utilities for pandemic
+
+1;
+__END__
+
+=head1 DESCRIPTION
+
+This module provides some helper subs for internationalizing pandemic.
+
